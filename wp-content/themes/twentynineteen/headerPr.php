9f25a91506a8cf31092398cbfababdccfa77523d
unknown <supertuxcar@gmail.com>
2024-03-07 23:02:52 +0100

    header bien puesto

---
+++ wp-content/themes/twentynineteen/headerPr.php
@@ -0,0 +1,37 @@
+    <!-- Menu de navegación-->
+
+    <header>
+        <div class="logo"><a href="#PagP"><img src="imagenes/logo_nuevo146x75.png" alt=""></a></div>
+        <div class="menuTlf">
+            <div class="linea"></div>
+            <div class="linea"></div>
+            <div class="linea"></div>
+        </div>
+        <nav class="nav-bar">
+            <ul>
+                <li class="">
+                    <a href="#eventos">Eventos</a>
+                </li>
+                <li class="">
+                    <a href="#primero">Quienes Somos</a>
+                </li>
+                <li class="">
+                    <a href="#segundo">Historia</a>
+                </li>
+                <li class="">
+                    <a href="#cuarto">Tutoriales</a>
+                </li>
+                <li class="">
+                    <a href="#tercero">Blog</a>
+                </li>
+                <li class="">
+                    <a href="tiendaProjectR.html">Tienda</a>
+                </li>
+                <li class="">
+                    <a href="#PagP" class="activa">Inicio</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Fin del Menu de navegación-->
--- wp-content/themes/twentynineteen/page-home.php
+++ wp-content/themes/twentynineteen/page-home.php
@@ -1,3 +1,8 @@
+<?php
+// Incluye el archivo headerPr.php desde el directorio del tema
+include(get_template_directory() . '/headerPr.php');
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -18,44 +23,6 @@
 </head>
 
 <body>
-
-    <!-- Menu de navegación-->
-
-    <header>
-        <div class="logo"><a href="#PagP"><img src="imagenes/logo_nuevo146x75.png" alt=""></a></div>
-        <div class="menuTlf">
-            <div class="linea"></div>
-            <div class="linea"></div>
-            <div class="linea"></div>
-        </div>
-        <nav class="nav-bar">
-            <ul>
-                <li class="">
-                    <a href="#eventos">Eventos</a>
-                </li>
-                <li class="">
-                    <a href="#primero">Quienes Somos</a>
-                </li>
-                <li class="">
-                    <a href="#segundo">Historia</a>
-                </li>
-                <li class="">
-                    <a href="#cuarto">Tutoriales</a>
-                </li>
-                <li class="">
-                    <a href="#tercero">Blog</a>
-                </li>
-                <li class="">
-                    <a href="tiendaProjectR.html">Tienda</a>
-                </li>
-                <li class="">
-                    <a href="#PagP" class="activa">Inicio</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-
-    <!-- Fin del Menu de navegación-->
 
     <!-- Menu para dispositivos moviles-->
 
